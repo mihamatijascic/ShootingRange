84e58af3913a5e09edc0da8d14d97099a63d4419
mihamatijascic <69255848+mihamatijascic@users.noreply.github.com>
2022-01-04 19:36:46 +0100

    add sound effect on gun

--- Assets/Scripts/GunBehaviour.cs
+++ Assets/Scripts/GunBehaviour.cs
@@ -10,22 +10,22 @@ public class GunBehaviour : MonoBehaviour
 
     [SerializeField] private float damage = 10f;
     [SerializeField] private float range = 100000f;
+    [SerializeField] private float fireRate = 15f;
+    [SerializeField] private float gunSoundStart = 0.25f;
 
     [SerializeField] public Camera firstPersonCamera;
     [SerializeField] public ParticleSystem shotEffect;
     [SerializeField] public ParticleSystem impactEffect;
-    private List<ParticleSystem> particleSystemStorage;
 
-    private void Start()
-    {
-        List<ParticleSystem> list = new List<ParticleSystem>(20);
-    }
+    [SerializeField] private AudioClip gunSound;
+
+    private float nextTimeToFire = 0f;
 
     private void Update()
     {
-
-        if (Input.GetMouseButtonDown(LEFTMOUSE))
+        if (Input.GetMouseButton(LEFTMOUSE) && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -33,6 +33,8 @@ public class GunBehaviour : MonoBehaviour
     private void Shoot()
     {
         shotEffect.Play();
+        PlayGunSound();
+
         if (Physics.Raycast(firstPersonCamera.transform.position, firstPersonCamera.transform.forward, out var hit, range))
         {
             Debug.Log(hit.transform.name);
@@ -43,5 +45,14 @@ public class GunBehaviour : MonoBehaviour
             ParticleSystem impactObj = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactObj.gameObject, 2f);
         }
+    }
+
+    private void PlayGunSound()
+    {
+        var go = new GameObject("gunSound");
+        var audioSource = go.AddComponent<AudioSource>();
+        audioSource.time = gunSoundStart;
+        audioSource.PlayOneShot(gunSound);
+        Destroy(go, 2f);
     }
 }
